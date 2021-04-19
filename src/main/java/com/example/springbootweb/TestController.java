a93d9da0c2927d1349eca7a86adb4adc7acc277c
haozj <870808051@qq.com>
2021-04-19 23:27:04 +0800

    github test push

--- src/main/java/com/example/springbootweb/TestController.java
+++ src/main/java/com/example/springbootweb/TestController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class TestController {
@@ -13,7 +12,7 @@ public class TestController {
     @RequestMapping("/test")
     public ModelAndView TestDemo() {
         ModelAndView modelAndView = new ModelAndView("hello");
-        modelAndView.addObject("test","teste");
+        modelAndView.addObject("test","你好");
         return modelAndView;
     }
 }
